96c35ff8b9bd4cf1a0bef951fccd36a245b9933a
Serkan <153859528+uluserkan@users.noreply.github.com>
2024-01-11 14:17:52 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+static void Main(string[] args)
+		{
+
+			//While
+			// 1 den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program. // Console.Write("Lütfen bir sayi giriniz: ");
+			// int sayi = int.Parse(Console.ReadLine());
+			// int sayac = 1;
+			// int toplam = 0;
+			// while (sayac<<<= sayi)
+			// {
+			//
+			//
+			// }
+			//toplam += sayac;
+			//sayac++;
+			// Console.WriteLine(toplam/sayi);
+			// // 'a' dan 'z' ye kadar tüm harfleri console a yazdır.
+			// char character = 'a';
+			// while (character <'z') // {
+			//
+			// }
+			//Console.Write(character); character++;
+			Console.Write("***** Foreach *****");
+			string[] arabalar = { "BMV", "Ford", "Toyota", "Nissan" };
+			foreach (var araba in arabalar)
+			{
+				Console.WriteLine(araba);
+			}
+			
+		}
